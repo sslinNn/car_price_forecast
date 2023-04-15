5c69b9d0aadc3cc03038f178608c95825b4e4291
sslinNn <morison1991@mail.ru>
2023-04-15 23:28:56 +0700

    в цикл засунул и парсер напрямую с сайта включил

--- maindrom.py
+++ maindrom.py
@@ -1,120 +1,198 @@
 import requests
 from bs4 import BeautifulSoup
 
-# headers = {
-#     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36'
-# }
-#
-# url = f'https://auto.drom.ru/all/page1/'
-# response = requests.get(url=url, headers=headers)
+headers = {
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36'
+}
+
+for i in range(2):
+    url = f'https://auto.drom.ru/all/page{i}/'
+    response = requests.get(url=url, headers=headers)
+
+    soup = BeautifulSoup(response.text, 'lxml')
 #
 # with open('index.html', 'w') as file:
 #     file.write(response.text)
 
-with open('index.html') as file:
-    src = file.read()
-soup = BeautifulSoup(src, 'lxml')
-
-car = soup.find('div', class_='css-1nvf6xk eojktn00').findAll('div', class_='css-13ocj84 e1icyw250')[5]
-
-data_list = []
-for data_elem in car:
-    try:
-        data = data_elem.text.replace(' ', '')
-    except AttributeError:
-        data = None
-    if '.css' in data:
-        pass
-    else:
-        data_list.append(data)
-
-strg = ' '.join(data_list)
-print(strg)
+# with open('index.html') as file:
+#     src = file.read()
+# soup = BeautifulSoup(src, 'lxml')
 
 
-Title = []
-for symbol in strg:
-    if symbol != ',':
-        Title.append(symbol)
-    else:
-        break
-Title = ''.join(Title)
+    for n in range(20):
+        car = soup.find('div', class_='css-1nvf6xk eojktn00').findAll('div', class_='css-13ocj84 e1icyw250')[n]
+
+        data_list = []
+        for data_elem in car:
+            try:
+                price = data_elem.text.replace(' ', '')
+            except AttributeError:
+                price = None
+            if '.css' in price:
+                pass
+            else:
+                data_list.append(price)
+
+        strg = ' '.join(data_list)
 
 
-
-strg = strg[len(Title) + 2::]
-year = strg[0:4]
-strg = strg[len(year)::]
+        Title = []
+        for symbol in strg:
+            if symbol != ',':
+                Title.append(symbol)
+            else:
+                break
+        Title = ''.join(Title)
 
 
 
-symbol = ''
-model = []
-for symbol in strg:
-    model.append(symbol)
-    if 'л' in symbol:
-        break
-model = ''.join(model)[:-2:]
+        strg = strg[len(Title) + 2::]
+        year = strg[0:4]
+        strg = strg[len(year)::]
 
 
 
-if len(model) > 6:
-    eC = model[:3:]
-    model = model[0:-4:]
-    strg = strg[len(eC) + len(model) + 2::]
-else:
-    eC = model[1::]
-    model = None
-    strg = strg[len(eC) + 2::]
+        symbol = ''
+        model = []
+        for symbol in strg:
+            model.append(symbol)
+            if 'л' in symbol:
+                break
+        model = ''.join(model)[:-2:]
 
 
 
-hp = []
-symbol = ''
-for symbol in strg:
-    hp.append(symbol)
-    if ')' in symbol:
-        break
+        if len(model) > 6:
+            eC = model[:3:]
+            model = model[0:-4:]
+            strg = strg[len(eC) + len(model) + 2::]
+        else:
+            eC = model[1::]
+            model = None
+            strg = strg[len(eC) + 2::]
 
 
-strg = strg[len(hp)+2::]
-hp = ''.join(hp)[3:6]
+
+        hp = []
+        symbol = ''
+        for symbol in strg:
+            hp.append(symbol)
+            if ')' in symbol:
+                break
 
 
-fuel = []
-symbol = ''
-for symbol in strg:
-    fuel.append(symbol)
-    if ',' in symbol:
-        break
-strg = strg[len(fuel)+1::]
-fuel = ''.join(fuel)[:-1:]
+        strg = strg[len(hp)+2::]
+        hp = ''.join(hp)[3:6]
 
-trans = []
-symbol = ''
-for symbol in strg:
-    trans.append(symbol)
-    if ',' in symbol:
-        break
-strg = strg[len(trans)+1::]
-trans = ''.join(trans)[:-1:]
 
-drive = []
-symbol = ''
-for symbol in strg:
-    drive.append(symbol)
-    if ',' in symbol:
-        strg = strg[len(drive) + 1::]
-        drive = ''.join(drive)[:-1:]
-        break
-    elif ' ' in symbol:
-        strg = strg[len(drive)::]
-        drive = ''.join(drive)[:-1:]
-        break
+        fuel = []
+        symbol = ''
+        for symbol in strg:
+            fuel.append(symbol)
+            if ',' in symbol:
+                break
+        strg = strg[len(fuel)+1::]
+        fuel = ''.join(fuel)[:-1:]
 
-if strg == 'новый':
-    odo = 0
-else:
-    odo = strg.replace('тыс. км', '').replace(' ', '')
+        trans = []
+        symbol = ''
+        for symbol in strg:
+            trans.append(symbol)
+            if ',' in symbol:
+                break
+        strg = strg[len(trans)+1::]
+        trans = ''.join(trans)[:-1:]
 
-print(f'{Title} * {year} * {model} * {eC} * {hp} * {fuel} * {trans} * {drive} * {odo} *')
+        drive = []
+        symbol = ''
+        for symbol in strg:
+            drive.append(symbol)
+            if ',' in symbol:
+                strg = strg[len(drive) + 1::]
+                drive = ''.join(drive)[:-1:]
+                break
+            elif ' ' in symbol:
+                strg = strg[len(drive)::]
+                drive = ''.join(drive)[:-1:]
+                break
+
+        if strg == 'новый':
+            odo = 0
+        else:
+            odo = strg.replace('тыс. км', '').replace(' ', '')
+
+
+
+        """------------------------------------------------------------------------------------------------------"""
+        car = soup.find('div', class_='css-1nvf6xk eojktn00').findAll('div', class_='css-1dkhqyq e1f2m3x80')[n]
+
+        priceList = []
+        for price_elem in car:
+            try:
+                price = price_elem.text.replace(' ', '')
+            except AttributeError:
+                price = None
+            if '.css' in price:
+                pass
+            else:
+                priceList.append(price)
+
+
+        priceSTR = ' '.join(priceList)
+
+        carPrice = []
+        symbol = ''
+        for symbol in priceSTR:
+            carPrice.append(symbol)
+            if '₽' in symbol:
+                break
+
+        priceSTR = priceSTR[len(carPrice)::]
+        carPrice = ' '.join(carPrice).replace(' ', '')[:-1:]
+
+
+        if priceSTR[:3:] == 'выс':
+            priceSTR = priceSTR[13::]
+            highPrice = 1
+        else:
+            highPrice = 0
+
+        if priceSTR[:3:] == 'хор':
+            priceSTR = priceSTR[13::]
+            goodPrice = 1
+        else:
+            goodPrice = 0
+
+        if priceSTR[:3:] == 'нор':
+            priceSTR = priceSTR[16::]
+            defltPrice = 1
+        else:
+            defltPrice = 0
+
+        if priceSTR[:3:] == 'отл':
+            priceSTR = priceSTR[14::]
+            greatPrice = 1
+        else:
+            greatPrice = 0
+
+        if priceSTR[:3:] == 'сня':
+            priceSTR = priceSTR[15::]
+            removed = 1
+        else:
+            removed = 0
+
+        if priceSTR[:3:] == 'мин':
+            strrr = priceSTR[16::]
+            carPriceMin = []
+            symbol = ''
+            for symbol in strrr:
+                carPriceMin.append(symbol)
+                if '₽' in symbol:
+                    break
+            carPriceMin = ' '.join(carPriceMin).replace(' ', '')[:-1:]
+            minPrice = 1
+        else:
+            carPriceMin = None
+            minPrice = 0
+
+        print(f'{Title} * {year} * {model} * {eC} * {hp} * {fuel} * {trans} * {drive} * {odo} * price - {carPrice} * minCarPrice - {carPriceMin} * highPrice - {highPrice} * goodPrice - {goodPrice} * defltPrice - {defltPrice} * removed - {removed} * minPrice - {minPrice}')
