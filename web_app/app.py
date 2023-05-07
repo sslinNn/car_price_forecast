4135d0df31509e3b041a64c819bd255c0d466d39
sslinNn <morison1991@mail.ru>
2023-05-07 15:48:28 +0700

    some bag fixes

--- web_app/app.py
+++ web_app/app.py
@@ -15,7 +15,6 @@ def index():
         CarOdo = request.form['CarOdo']
         CarMark = request.form['CarMark']
         CarModel = request.form['CarModel']
-
         CarTrans = request.form['CarTrans']
         FuelType = request.form['FuelType']
         CarDrive = request.form['CarDrive']
@@ -31,25 +30,45 @@ def index():
                         car_model=CarModel
                         )
 
-        return render_template('result.html',
-                               CarYear=CarYear,
-                               EngineCapacity=EngineCapacity,
-                               HorsePower=HorsePower,
-                               CarOdo=CarOdo,
-                               CarTrans=CarTrans,
-                               FuelType=FuelType,
-                               CarDrive=CarDrive,
-                               CarMark=CarMark,
-                               CarModel=CarModel,
-                               result=result
-                               )
+        return redirect(url_for('result',
+                        CarYear=CarYear,
+                        EngineCapacity=EngineCapacity,
+                        HorsePower=HorsePower,
+                        CarOdo=CarOdo,
+                        CarTrans=CarTrans,
+                        FuelType=FuelType,
+                        CarDrive=CarDrive,
+                        CarMark=CarMark,
+                        CarModel=CarModel,
+                        result=result))
     else:
         return render_template('index.html')
 
 
 @app.route('/result')
 def result():
-    return render_template('result.html')
+    CarYear = request.args.get('CarYear'),
+    EngineCapacity = request.args.get('EngineCapacity'),
+    HorsePower = request.args.get('HorsePower'),
+    CarOdo = request.args.get('CarOdo'),
+    CarTrans = request.args.get('CarTrans'),
+    FuelType = request.args.get('FuelType'),
+    CarDrive = request.args.get('CarDrive'),
+    CarMark = request.args.get('CarMark'),
+    CarModel = request.args.get('CarModel'),
+    result = request.args.get('result')
+    return render_template('result.html',
+                           CarYear=CarYear[0],
+                           EngineCapacity=EngineCapacity[0],
+                           HorsePower=HorsePower[0],
+                           CarOdo=CarOdo[0],
+                           CarTrans=CarTrans[0],
+                           FuelType=FuelType[0],
+                           CarDrive=CarDrive[0],
+                           CarMark=CarMark[0],
+                           CarModel=CarModel[0],
+                           result=result[:-2:]
+                           )
 
 
 if __name__ == '__main__':
