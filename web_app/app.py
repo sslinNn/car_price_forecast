d953ae9b90aa8f2307c41684ad5bb85bc525b590
sslinNn <morison1991@mail.ru>
2023-05-12 00:16:53 +0700

    POST, GET request fix

--- web_app/app.py
+++ web_app/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 
 @app.route('/', methods=['GET', 'POST'])
-@app.route('/home', methods=['GET', 'POST'])
+@app.route('/home', methods=['POST'])
 def index():
     if request.method == 'POST':
         CarYear = request.form['CarYear']
@@ -19,55 +19,54 @@ def index():
         FuelType = request.form['FuelType']
         CarDrive = request.form['CarDrive']
 
-        result = mms.ml(car_year=CarYear,
-                        car_odo=CarOdo,
-                        engine_capacity=EngineCapacity,
-                        horse_power=HorsePower,
-                        car_trans=CarTrans,
-                        fuel_type=FuelType,
-                        car_drive=CarDrive,
-                        car_mark=CarMark,
-                        car_model=CarModel
-                        )
-
-        return redirect(url_for('result',
-                        CarYear=CarYear,
-                        EngineCapacity=EngineCapacity,
-                        HorsePower=HorsePower,
-                        CarOdo=CarOdo,
-                        CarTrans=CarTrans,
-                        FuelType=FuelType,
-                        CarDrive=CarDrive,
-                        CarMark=CarMark,
-                        CarModel=CarModel,
-                        result=result))
+        with open('data.txt', 'w') as f:
+            f.write(f"{CarYear},{EngineCapacity},"
+                    f"{HorsePower},{CarOdo},"
+                    f"{CarMark},{CarModel},"
+                    f"{CarTrans},{FuelType},{CarDrive}")
+        return redirect(url_for('result'))
     else:
         return render_template('index.html')
 
 
 @app.route('/result')
 def result():
-    CarYear = request.args.get('CarYear'),
-    EngineCapacity = request.args.get('EngineCapacity'),
-    HorsePower = request.args.get('HorsePower'),
-    CarOdo = request.args.get('CarOdo'),
-    CarTrans = request.args.get('CarTrans'),
-    FuelType = request.args.get('FuelType'),
-    CarDrive = request.args.get('CarDrive'),
-    CarMark = request.args.get('CarMark'),
-    CarModel = request.args.get('CarModel'),
-    result = request.args.get('result')
+    with open("data.txt", 'r') as f:
+        data = f.read()
+    data = data.split(sep=',')
+
+
+    CarYear = data[0]
+    EngineCapacity = data[1]
+    HorsePower = data[2]
+    CarOdo = data[3]
+    CarMark = data[4]
+    CarModel = data[5]
+    CarTrans = data[6]
+    FuelType = data[7]
+    CarDrive = data[8]
+
+    res = mms.ml(car_year=CarYear,
+                 car_odo=CarOdo,
+                 engine_capacity=EngineCapacity,
+                 horse_power=HorsePower,
+                 car_trans=CarTrans,
+                 fuel_type=FuelType,
+                 car_drive=CarDrive,
+                 car_mark=CarMark,
+                 car_model=CarModel)
+
     return render_template('result.html',
-                           CarYear=CarYear[0],
-                           EngineCapacity=EngineCapacity[0],
-                           HorsePower=HorsePower[0],
-                           CarOdo=CarOdo[0],
-                           CarTrans=CarTrans[0],
-                           FuelType=FuelType[0],
-                           CarDrive=CarDrive[0],
-                           CarMark=CarMark[0],
-                           CarModel=CarModel[0],
-                           result=result[:-2:]
+                           CarYear=CarYear,
+                           EngineCapacity=EngineCapacity,
+                           HorsePower=HorsePower,
+                           CarOdo=CarOdo,
+                           CarTrans=CarTrans,
+                           FuelType=FuelType,
+                           CarDrive=CarDrive,
+                           CarMark=CarMark,
+                           CarModel=CarModel,
+                           res=res
                            )
 
 
