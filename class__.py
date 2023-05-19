f24e429f43d8cf17308d39d94c56087601eeb774
sslinNn <113080924+sslinNn@users.noreply.github.com>
2023-05-20 01:15:16 +0700

    object serialization

--- class__.py
+++ class__.py
@@ -11,13 +11,13 @@ class Car:
         self.CarDrive = CarDrive
 
     def get_info(self):
-        info = f"CarMark: {self.CarMark};" \
-               f"CarModel: {self.CarModel};" \
-               f"CarYear: {self.CarYear};" \
-               f"EngineCapacity: {self.EngineCapacity};" \
-               f"HorsePower: {self.HorsePower};" \
-               f"CarOdo: {self.CarOdo};" \
-               f"CarTrans: {self.CarTrans};" \
-               f"FuelType: {self.FuelType};" \
-               f"CarDrive: {self.CarDrive};"
-        return info.split(sep=';')
+        info = f"CarMark:{self.CarMark}; " \
+               f"CarModel:{self.CarModel}; " \
+               f"CarYear:{self.CarYear}; " \
+               f"EngineCapacity:{self.EngineCapacity}; " \
+               f"HorsePower:{self.HorsePower}; " \
+               f"CarOdo:{self.CarOdo}; " \
+               f"CarTrans:{self.CarTrans}; " \
+               f"FuelType:{self.FuelType}; " \
+               f"CarDrive:{self.CarDrive}; "
+        return info
